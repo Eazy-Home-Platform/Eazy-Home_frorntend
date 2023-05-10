98ca83c01b7c5774894a4813f32b2fddb7d0bc78
Elua-b <bugingoeloi@gmail.com>
2023-05-10 09:56:22 +0200

    adding routes

--- app/page.tsx
+++ app/page.tsx
@@ -1,33 +1,17 @@
+"use client";
 import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import Logo from './components/Logo'
-import Navbar from './components/Navbar'
-import AuthButtons from './components/AuthButtons'
-import Category from './components/Category'
-import Listings from './components/Listings'
-
-const inter = Inter({ subsets: ['latin'] })
+import HomePage from './components/HomePage'
+import {BrowserRouter,Routes,Route } from 'react-router-dom'
 
 export default function Home() {
   return (
-    <main className="flex min-h-[100vh] flex-col  justify-between p-2">
-      <div className="bg-[url('/landing-image.jpeg')] w-full bg-fixed bg-cover rounded-md h-[98vh] box-border">
-      <div className="container-gradient">
-        <div className="mx-auto flex flex-row items-center justify-around py-5">
-          <Logo />
-          <Navbar />
-          <AuthButtons />
-        </div>
-        <div className="w-full h-screen flex flex-col items-center justify-center gap-10">
-          <div className="font-bold text-4xl">Helping others</div>
-          <div className="font-bold text-6xl">LIVE & TRAVEL</div>
-          <div className="font-semibold text-2xl">Special offers to suite your plan</div>
-        </div>
-      </div>
-      </div>
-      <Category />
-      <Listings />
-    </main>
+<div>
+  <BrowserRouter>
+  <Routes>
+    <Route path='/' element={<HomePage/>}/>
+  </Routes>
+  </BrowserRouter>
+</div>
     
   )
 }
