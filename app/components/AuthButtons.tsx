45de390046723c156c88515e968fab3ec36bbefb
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2023-05-11 15:49:16 +0200

    [feat:] project updates

--- app/components/AuthButtons.tsx
+++
@@ -1,9 +0,0 @@
-const AuthButtons = () => {
-    return (
-        <div className="flex flex-row justify-center items-center gap-5">
-            <div className="font-semibold">Login</div>
-            <div className="p-3 rounded bg-white text-black font-semibold">Sign Up</div>
-        </div>
-    )
-}
-export default AuthButtons
