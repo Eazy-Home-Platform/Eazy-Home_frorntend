8d82e599fed56122d0bd54035df9809ab61c6f3e
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2023-05-11 21:11:37 +0200

    [feat]: account info data display profile

--- app/account-pages/(features)/account-info-data.tsx
+++ app/account-pages/(features)/account-info-data.tsx
@@ -3,15 +3,15 @@ import { FaEdit } from 'react-icons/fa'
 
 const AccountInfoData = ({ value ,label}: { label: any,value: any}) => {
     return (
-        <div className='flex justify-between place-items-center'>
+        <div className='flex justify-between py-2 place-items-center'>
             <div>
                 <label className='text-[12px] text-slate-500'>{label}</label>
-                <h1 className='text-black font-semibold text-[18px]'>{value}</h1>
+                <h1 className='text-black text-[16px]'>{value}</h1>
             </div>
             <div>
-                <button className='bg-white border-btn_color text-black px-10 py-4 border-2 border-sm'>
-                    <FaEdit />
-                    <span>Change</span>
+                <button className='bg-white border-btn_color text-black px-10 text-[12px] hover:bg-btn_color duration-300 flex justify-center gap-6 py-3 border-2 rounded-sm'>
+                    <FaEdit/>
+                    <span className='text-[12px]'>Change</span>
                 </button>
             </div>
         </div>
