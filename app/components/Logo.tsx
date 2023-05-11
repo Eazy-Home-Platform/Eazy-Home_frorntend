1064f9adbe02dc0768b4ff91a0a146199b1be29a
edmondgaks <edmondgaks@gmail.com>
2023-05-11 10:50:23 +0200

    small fixations

--- app/components/Logo.tsx
+++ app/components/Logo.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 const Logo = () => {
     return (
         <div>
-            <Image src="/logo.png" width={60} height={40} alt="LOGO" />
+            <Image src="/logo.png" width={100} height={40} alt="LOGO" />
         </div>
     )
 }
--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -3,17 +3,17 @@ import { BsFillTelephoneFill } from "react-icons/bs";
 import Link from "next/link";
 const Navbar = () => {
     return (
-        <div className="flex flex-row items-center justify-center gap-20">
+        <div className="flex flex-row items-center justify-center gap-10 text-sm">
             <div className="flex flex-row gap-2">
-                <AiFillHome size={24} color="white" />
+                <AiFillHome size={20} color="white" />
                 <Link href="/" className="font-bold">Home </Link>
             </div>
             <div className="flex flex-row gap-2">
-                <AiFillInfoCircle size={24} color="white" />
+                <AiFillInfoCircle size={20} color="white" />
                 <Link href="about" className="font-bold">About Us</Link>
             </div>
             <div className="flex flex-row gap-2">
-                <BsFillTelephoneFill size={24} color="white" />
+                <BsFillTelephoneFill size={20} color="white" />
                 <Link href="contact" className="font-bold">Contact Us</Link>
             </div>
         </div>
