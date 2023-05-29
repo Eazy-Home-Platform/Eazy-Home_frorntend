9e466396646eef0a651868429b2f0222d53a391e
edmondgaks <edmondgaks@gmail.com>
2023-05-29 10:47:59 +0200

    adding some of payment

---
+++ app/Landing/landing.tsx
@@ -0,0 +1,27 @@
+"use client";
+import AuthButtons from "../(components)/AuthButtons"
+import Logo from "../(components)/Logo"
+import Navbar from "../(components)/Navbar"
+
+const Landing = () => {
+    return (
+        <div className="p-2">
+        <div className="bg-[url('/landing-image.jpeg')] w-full bg-fixed bg-cover rounded-md h-[98vh] box-border">
+          <div className="container-gradient">
+            <div className="mx-auto flex flex-row items-center justify-around py-5">
+              <Logo />
+              <Navbar />
+              <AuthButtons />
+            </div>
+            <div className="w-full h-screen flex flex-col items-center justify-center gap-10">
+              <div className="font-bold text-4xl">Helping others</div>
+              <div className="font-bold text-6xl">LIVE & TRAVEL</div>
+              <div className="font-semibold text-2xl">Special offers to suite your plan</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+}
+
+export default Landing
---
+++ app/payment/page.tsx
@@ -0,0 +1,33 @@
+"use client";
+import React from "react";
+import Footer from "../(components)/Footer"
+import Header from "../(components)/header"
+import Image from "next/image";
+import { TextField } from "@mui/material";
+
+const Payment = () => {
+    return (
+        <div className="bg-[#FAFBFC] text-black min-h-screen">
+            <Header />
+            <div className="w-full flex flex-row">
+                <div className="w-[40%]">
+                    <Image src="/fav3.jpeg" width={250} height={600} alt="properties" className="rounded-lg" />
+                </div>
+                <div className="w-[70%]">
+                    <div className="text-lg font-bold">Your book details</div>
+                    <div className="flex flex-row w-[90%] justify-around p-5 shadow-md">
+                        <TextField id="outlined-basic" label="Check in" variant="outlined" />
+                        <TextField id="outlined-basic" label="Check out" variant="outlined" />
+                    </div>
+                    <div className="text-lg font-bold">Chose how to play</div>
+                    <div className="w-[90%] flex shadow-md">
+
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Payment
