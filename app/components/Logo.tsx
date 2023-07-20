3da458019d77a2906aa306f5e200aa40e8738ba7
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2023-07-20 21:31:03 +0200

    [fix]: conflicts resolution --unnecessary-files

--- app/components/Logo.tsx
+++
@@ -1,9 +0,0 @@
-import Image from "next/image";
-const Logo = () => {
-    return (
-        <div>
-            <Image src="/logo.png" width={100} height={40} alt="LOGO" />
-        </div>
-    )
-}
-export default Logo
