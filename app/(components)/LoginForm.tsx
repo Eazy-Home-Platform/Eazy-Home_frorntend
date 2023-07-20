ba8e5eeb181e4e5c63189f5c7da6400a1be5a0d3
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2023-07-20 22:37:05 +0200

    [add]: apos fixing

--- app/(components)/LoginForm.tsx
+++ app/(components)/LoginForm.tsx
@@ -21,17 +21,16 @@ const LoginForm = () => {
     }
     return (
         <div className="w-full flex flex-col    p-4">
-            <h2 className="font-normal text-[20px]  pl-5">Login</h2>
+            <h2 className="font-normal text-[20px] pl-5">Login</h2>
             <p className="font-normal text-[16px] pl-5 text-[#112211] pt-2">
-                Let&apos;s get you all st up so you can access your personal account.{" "} </p>
-            <form action="" className="p-4 ">
+                Let&apos;s get you all st up so you can access your personal account. </p>
+            <form action="" method="post" className="p-4 ">
                 <div className="flex pt-5 justify-center items-center w-full gap-3"></div>
                 <div className="flex pt-5 justify-center items-center w-full gap-3">
                     <div className="flex flex-col justify-center w-full">
                         <label className="text-[#79747E]">Email</label>
                         <input type="email" className="border-[1px] p-2 border-solid border-[#79747E] rounded-sm bg-transparent" placeholder=""/>
                     </div>
-
                 </div>
                 <div className="flex pt-5 flex-col justify-center">
                     <div className="flex flex-col w-full">
@@ -57,7 +56,7 @@ const LoginForm = () => {
                 <button type="submit" className=" mt-2 p-3 px-40 w-full bg-[#8DD3BB] text-black rounded-[4px] ">
                     Create account</button>
                 <p className="text-center pt-2">
-                    Don't have an account?
+                    Don&apos;t have an account?
                     <a className="text-red-400" href="">Login
                     </a>
                 </p>
@@ -70,9 +69,7 @@ const LoginForm = () => {
                     <div className="flex justify-between items-center w-full">
                         <i className=" py-4 px-20 border-[2px] cursor-pointer border-green-100"><BsFacebook className="text-blue-600 h-6 w-6"/></i>
                         <i className="py-4 px-20 border-[2px] cursor-pointer border-green-100"><FcGoogle className="h-6 w-6"/></i>
-
                         <i className="py-4 px-20 border-[2px] cursor-pointer   border-green-100"><BsApple className="h-6 w-6"/></i>
-
                     </div>
                     <div></div>
                     <div></div>
